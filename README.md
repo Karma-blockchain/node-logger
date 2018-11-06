e9cd564800e9ab92889c8a8d2732a05ea5789aec
Nikolay Nozdrin-Plotnitsky <nozdrin.plotnitsky@karma.red>
2018-11-06 13:52:33 +0300

    init commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Logger for nodejs service
+
+## Use dotenv variables
+```
+LOGSTASH_APP_NAME=karma-logger
+LOGSTASH_MSG_TYPE=nodejs-logger
+LOG_LEVEL=info
+
+FILE_LOG=true
+FILE_LOG_PATH=debug.log
+```
---
+++ index.js
@@ -0,0 +1,55 @@
+const
+  winston = require("winston"),
+  logform = require("logform"),
+  os = require('os'),
+  { combine, timestamp, printf } = logform.format;
+
+require("dotenv").config();
+
+module.exports = label => {
+  let appName = process.env.LOGSTASH_APP_NAME || process.cwd().split("/").slice(-1).pop()
+  let format = data => JSON.stringify({
+    '@timestamp': data.timestamp,
+    '@version': '1',
+    'message': data.message,
+    'host': os.hostname(),
+    'path': process.cwd(),
+    'tags': [],
+    'type': process.env.LOGSTASH_MSG_TYPE || 'nodejs-logger',
+
+    // Extra Fields
+    'level': data.level,
+    'logger_name': label,
+    'app': appName
+  })
+
+  let transports = [
+    new winston.transports.Console({
+      level: process.env.LOG_LEVEL || 'info',
+      format: combine(
+        timestamp(),
+        printf(format)
+      )
+    })
+  ]
+
+  if (process.env.FILE_LOG.toLowerCase() == 'true')
+    transports.push(
+      new winston.transports.File({
+        level: process.env.LOG_LEVEL || 'info',
+        filename: process.env.FILE_LOG_PATH || 'debug.log',
+        maxsize: 10000000,
+        format: combine(
+          timestamp(),
+          printf(data => `${data.timestamp} [${data.level}] ${appName} ${data.message}`)
+        ),
+        json: false
+      })
+    )
+
+  return winston.createLogger({
+    level: process.env.LOG_LEVEL || 'info',
+    //levels: {notify: 0, error: 1, warn: 2, info: 3},
+    transports
+  });
+}
